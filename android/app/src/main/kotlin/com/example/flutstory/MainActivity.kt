6efe0b1cf4b7818519b3548c2e3ceb592d0724d8
masbroustudio <masbroumail@gmail.com>
2024-04-25 21:39:13 +0700

    success#01

---
+++ android/app/src/main/kotlin/com/example/flutstory/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutstory
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/data/network/api_endpoints.dart
@@ -0,0 +1,8 @@
+class ApiEndpoints {
+  static const baseUrl = 'story-api.dicoding.dev';
+  static const apiVersion = '/v1';
+
+  static const login = '$apiVersion/login';
+  static const register = '$apiVersion/register';
+  static const stories = '$apiVersion/stories';
+}
